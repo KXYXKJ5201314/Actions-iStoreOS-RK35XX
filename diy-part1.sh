9c7981edd1114a92ce0847e1f5ba8eb41a95607b
空心游戏科技 <161995786+KXYXKJ5201314@users.noreply.github.com>
2025-02-28 10:41:52 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,3 +24,10 @@ echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.con
 
 # passwall2插件
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >> feeds.conf.default
+
+
+# 更新 feeds
+./scripts/feeds update packages
+
+# 安装 luci-i18n-passwall2-zh-cn
+./scripts/feeds install luci-i18n-passwall2-zh-cn
